32282488f2977978e027315eb40a193e5862e3cd
陈灵灵 <798092775@qq.com>
2019-04-04 18:36:40 +0800

    img

--- src/views/vision/specialPoint.vue
+++ src/views/vision/specialPoint.vue
@@ -293,13 +293,13 @@ export default {
   .container-case-img {
     width: 90px;
     height: 60px;
-    border-radius: 4px;
     background: pink;
     margin: 10px 0;
     img {
       width: 100%;
       height: 100%;
-      overflow: hidden;
+      object-fit: cover;
+      border-radius: 4px;
     }
   }
 
