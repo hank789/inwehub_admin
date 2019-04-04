6272689afe46200ea49ae3ef169758e6ea800ee4
陈灵灵 <798092775@qq.com>
2019-04-04 18:30:56 +0800

    获取产品Id

--- src/views/vision/specialPoint.vue
+++ src/views/vision/specialPoint.vue
@@ -120,8 +120,9 @@ export default {
     }
   },
   created() {
-    this.$store.dispatch('product/getProductInfo').then((product) => {
+    this.$store.dispatch('product/getProductInfo', (product) => {
       this.listQuery.product_id = product.id
+
       this.getList()
     })
   },
